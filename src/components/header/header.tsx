0bff97f94ad3405fb5f952ee84c3e85c31dac97c
G9 Aerospace <g9militants@gmail.com>
2024-04-23 10:41:41 +0530

    Update header styles and add LegacyNodes FAQ title

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,15 +8,15 @@ export default component$(() => {
   return (
     <header class={`${styles.header}`}>
       <div class={styles.wrapper}>
-
-          <div class={styles.logo}>
-            <img src={legacynodeslogourl} alt="LegacyNodes Logo" width="100" height="100" />
-          </div>
-
-          <ul class={styles.navList}>
-            <li><a href="/">Home</a></li>
-          </ul>
-          
+        <div class={styles.logo}>
+          <img src={legacynodeslogourl} alt="LegacyNodes Logo" width="100" height="100" />
+        </div>
+        <div class={styles.title}>
+          LegacyNodes FAQ
+        </div>
+        <ul class={styles.navList}>
+          <li><a href="/">Home</a></li>
+        </ul>
       </div>
     </header>
   );
